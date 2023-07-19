0086e4ec3f23752e6b5327684ccab7e6100fc960
Sahinshf <shahinrsha@code.edu.az>
2023-07-20 00:51:23 +0400

    Task is added

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Task2;
+
+ class Program
+{
+    static void Main(string[] args)
+    {
+        
+        int sqrResult = Power(int.Parse(Console.ReadLine()));
+        int pwrResult = Power(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+        Console.WriteLine(sqrResult+ pwrResult);
+
+    }
+    static int Power(int num)
+    {
+        return num * num; 
+    }
+
+    static int Power(int num1, int num2)
+    {
+        return (int)Math.Pow(num1, num2); 
+    }
+}
